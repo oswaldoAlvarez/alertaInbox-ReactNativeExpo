051b4723753d098172020a593815b60c7a88224a
Oswaldo Alvarez <oswaldo.alvarez.d95@gmail.com>
2025-06-30 17:42:38 +0200

    chore: adding fade in for notification

--- src/notifications/view/notificationCard/NotificationCard.component.tsx
+++ src/notifications/view/notificationCard/NotificationCard.component.tsx
@@ -3,8 +3,11 @@ import { TextView } from "@/src/shared/ui/textView/TextView.component";
 import { useThemeToggle } from "@/src/theme/ThemeContext";
 import React from "react";
 import { TouchableOpacity, View } from "react-native";
+import Animated, { FadeIn } from "react-native-reanimated";
 import { Notification } from "../../model/notification.types";
 import { styles } from "./NotificationCard.styles";
+
+const FADE_IN_DURATION = 1500;
 
 type INotificationCard = {
   notification: Notification;
@@ -18,36 +21,38 @@ export const NotificationCard = ({
   const { colors, isDark } = useThemeToggle();
 
   return (
-    <TouchableOpacity
-      onPress={onPress}
-      style={[
-        styles.container,
-        {
-          backgroundColor: notification.read
-            ? colors.card
-            : colors.notificationRead,
-          borderColor: colors.border,
-        },
-      ]}
-    >
-      <View style={styles.icon}>{iconByType[notification.type]}</View>
-      <View>
-        <TextView textStyles={styles.title}>{notification.title}</TextView>
-        <TextView
-          numberOfLines={1}
-          textStyles={[
-            styles.description,
-            { color: isDark ? colors.text : "#444" },
-          ]}
-        >
-          {notification.description}
-        </TextView>
-        <TextView
-          textStyles={[styles.date, { color: isDark ? colors.text : "grey" }]}
-        >
-          {notification.date}
-        </TextView>
-      </View>
-    </TouchableOpacity>
+    <Animated.View entering={FadeIn.duration(FADE_IN_DURATION)}>
+      <TouchableOpacity
+        onPress={onPress}
+        style={[
+          styles.container,
+          {
+            backgroundColor: notification.read
+              ? colors.card
+              : colors.notificationRead,
+            borderColor: colors.border,
+          },
+        ]}
+      >
+        <View style={styles.icon}>{iconByType[notification.type]}</View>
+        <View>
+          <TextView textStyles={styles.title}>{notification.title}</TextView>
+          <TextView
+            numberOfLines={1}
+            textStyles={[
+              styles.description,
+              { color: isDark ? colors.text : "#444" },
+            ]}
+          >
+            {notification.description}
+          </TextView>
+          <TextView
+            textStyles={[styles.date, { color: isDark ? colors.text : "grey" }]}
+          >
+            {notification.date}
+          </TextView>
+        </View>
+      </TouchableOpacity>
+    </Animated.View>
   );
 };
